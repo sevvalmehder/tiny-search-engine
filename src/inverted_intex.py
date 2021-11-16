168ab1a3bc9cc15ce9a5a4b5059fed8e49e2b2ae
sevval.mehder <sevval.mehder@inventanalytics.com>
2021-11-16 22:36:21 +0300

    operators are implemented

--- src/inverted_intex.py
+++ src/inverted_intex.py
@@ -53,8 +53,70 @@ class InvertedIndex(BaseInvertedIndex):
             f.close
         print("[Done] Dictionary is loaded!")
 
-    def merge(self):
-        pass
-    
-    def union(self):
-        pass
+    def merge(self, l, r):
+        """
+        Return the intersection of the left(l) and right(r) postings
+
+        It represents the AND operation.
+        """
+        # Initialize indexes and result as an empty list
+        # i represents the pointer of long list and j belongs to short one
+        i, j = 0, 0
+        result = []
+
+        short, long = (l, r) if len(l) < len(r) else (r, l)
+        while i < len(long) and j < len(short):
+            if long[i] == short[j]:
+                result.append(long[i])
+                j += 1
+                i += 1
+            elif long[i] > short[j]:
+                j += 1
+            else:
+                i += 1
+            
+
+        return result
+
+    def union(self, l, r):
+        """
+        Return the union of the left(l) and right(r) postings
+
+        It represents the OR operation.
+        """
+        # Initialize indexes and result as an empty list
+        # i represents the pointer of long list and j belongs to short one
+        i, j = 0, 0
+        result = []
+
+        short, long = (l, r) if len(l) < len(r) else (r, l)
+
+        # This loop ends when short list traversed
+        while i < len(long) and j < len(short):
+            if long[i] <= short[j]:
+                result.append(long[i])
+                if long[i] == short[j]:
+                    j += 1
+                i += 1
+            else:
+                result.append(short[j])
+                j += 1
+        
+        # We may still have elements in long list.
+        # These remain elements are added to result
+
+        return result + long[i:]
+
+    def difference(self, l, r):
+        """
+        Return the difference of the left(l) and right(r) postings
+
+        It represents the NOT operation. (l-r or l/r)
+        """
+        result = l.copy()
+
+        for i in r:
+            if i in result:
+                result.remove(i)
+        
+        return result
